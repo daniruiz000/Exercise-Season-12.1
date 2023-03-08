6885cc41625cda62ae56d8abe7d94db3a2795049
Daniel Ruiz <danielruimar@gmail.com>
2023-03-08 13:28:53 +0100

    Optimized Renders

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import './Product.css';
 
-const Product = (props) => {
-
+const Product = React.memo((props) => {
+    console.log('Renderiza Product '+ props.product.productName)
     return (
         <div className= 'shopping-cart__product' key={props.product.id}>
-            <li >{props.product.productName} / {props.product.amount} unidades / a {props.product.price}€ cada una</li>
+            <li >{props.product.productName} / {props.product.amount} unidades / a {props.product.price}€ cada unidad</li>
             <button onClick={() => props.handleClick(props.product.id)}>Eliminar</button>
         </div>
     )
-}
+})
 
 export default Product;
